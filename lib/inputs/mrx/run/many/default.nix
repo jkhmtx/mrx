491aaaad481a5842641b6ed741628c9d07861690
Jake Hamilton <31600776+jkhmtx@users.noreply.github.com>
2026-01-20 18:30:48 -0600

    feat(nix): add 'keepGoing' flag to _.mrx.run.many (#27)

--- lib/inputs/mrx/run/many/default.nix
+++ lib/inputs/mrx/run/many/default.nix
@@ -6,33 +6,61 @@
   name,
   each,
   extraRuntimeEnv ? {},
+  keepGoing ? false,
 }: let
-  inherit (builtins) readFile typeOf;
-  inherit (nixpkgs.lib) getExe;
+  inherit (builtins) elem isBool readFile typeOf;
   inherit (nixpkgs.lib.attrsets) isDerivation;
   inherit (nixpkgs.lib.lists) any imap0;
-  inherit (nixpkgs.lib.strings) join;
-  inherit (nixpkgs.lib.trivial) throwIf;
-  inherit (utils.fp) filterLeft left right filterRight isLeft;
+  inherit (nixpkgs.lib.strings) getName join;
+  inherit (nixpkgs.lib.trivial) boolToString throwIf;
+  inherit (utils.fn) not;
+  inherit (utils.fp) filterLeft left right getRight isLeft;
 
-  exe = i: drv:
+  mapLeftErrInfo = i: drv:
     if isDerivation drv
-    then right (getExe drv)
+    then right drv
     else left "index ${toString i}: ${typeOf drv}";
 
-  eithers = imap0 exe each;
+  eithers = imap0 mapLeftErrInfo each;
 
-  assertGetExes = list: let
+  assertAllDerivations = list: let
     invalidTypesStr = join "\n  " (filterLeft list);
-    assertExes = throwIf (any isLeft list) "_.mrx.run.many: Members of `each` must be of type 'derivation'. Got:\n  ${invalidTypesStr}";
+    validate = throwIf (any isLeft list) "_.mrx.run.many: Members of `each` must be of type 'derivation'. Got:\n  ${invalidTypesStr}";
   in
-    assertExes (filterRight list);
+    validate list;
+
+  toStringOrBoolString = v:
+    if isBool v
+    then boolToString v
+    else toString v;
+
+  assertKeepGoing = strKeepGoing: let
+    allowed = ["true" "false" "no-stderr-summary"];
+
+    validate = throwIf (not (elem strKeepGoing) allowed) "'keepGoing' may be skipped, or provided any of: [${join ", " allowed}]. Got: '${strKeepGoing}'";
+  in
+    validate strKeepGoing;
+
+  mode = let
+    modes = {
+      false = "no-keep-going";
+      true = "keep-going";
+      "no-stderr-summary" = "keep-going-no-summary";
+    };
+
+    strKeepGoing = toStringOrBoolString keepGoing;
+  in
+    modes.${assertKeepGoing strKeepGoing};
+
+  derivations = map getRight (assertAllDerivations eithers);
 in
   nixpkgs.writeShellApplication {
     inherit name;
+    runtimeInputs = derivations ++ [nixpkgs.coreutils];
     runtimeEnv =
       {
-        LIST = assertGetExes eithers;
+        LIST = map getName derivations;
+        MODE = mode;
       }
       // extraRuntimeEnv;
 
--- scripts/bin/fix/main.nix
+++ scripts/bin/fix/main.nix
@@ -2,6 +2,8 @@
 _.mrx.run.many {
   name = import _/name;
 
+  keepGoing = "no-stderr-summary";
+
   each = [
     _.format
     _.lint
